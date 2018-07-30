607538f6f71884c3e4527265374b0e38d2ad11ae
Unknown <mgijon94@gmail.com>
2018-07-31 00:03:13 +0200

    updated

--- PHP/lifeCicle.php
+++ PHP/lifeCicle.php
@@ -1,5 +1,7 @@
 <?php
 
+/* First example */
+
 class Animal {
 	function __construct() {
 		echo("Constructed!! \n");
@@ -23,5 +25,25 @@ $y = new Animal();
 echo ("-- THREE -- \n");
 
 x-> something();
+/* if the programm ends here the objects x and y would be destroed, the garbage collector calls the __destruct function */
+
+/* Second example */
+
+class Hello {
+	protected $lang; // only accesible in the class
+
+	function __construct($lang){
+		$this -> lang = $lang;
+	}
+	function greet() {
+		if ($this -> lang == 'fr') return 'Bonjour';
+		if ($this -> lang == 'es') return 'Hola';
+		return 'Hello'; 
+	}
+}
+
+$hi = new Hello('es');
+echo $hi->greet()."\n";
+
 
 >
