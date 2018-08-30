34ae69163f3a7d94f85833d392258731546554ea
Unknown <mgijon94@gmail.com>
2018-08-30 23:56:21 +0200

    updated

--- Vue/airsoft-galery/src/App.vue
+++ Vue/airsoft-galery/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <!-- ahora insertamos aquí el contenido del router view -->
     <router-view></router-view>
   </div>
 </template>
