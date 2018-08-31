535e459c3c341409310a30bd44dfef396ce28e60
Unknown <mgijon94@gmail.com>
2018-08-31 12:51:01 +0200

    updated

--- Vue/airsoft-galery/src/App.vue
+++ Vue/airsoft-galery/src/App.vue
@@ -1,8 +1,37 @@
 <template>
   <div id="app">
     <!-- ahora insertamos aquí el contenido del router view -->
-    <router-view></router-view>
-  </div>
+  <div>
+        <nav class="navbar navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar">Home</span>
+                        <span class="icon-bar">History</span>
+                        <span class="icon-bar">About</span>
+                        <span>Contact</span>
+                    </button>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <router-link tag="li" to="/" exact>
+                            <a>Home</a>
+                        </router-link>
+                        <router-link tag="li" to="/About">
+                            <a>Acerca de...</a>
+                        </router-link>
+                        <router-link tag="li" to="/contact">
+                            <a>Contacto</a>
+                        </router-link>                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+        <router-view></router-view>
+  </div> <!-- end app -->
 </template>
 
 <script>
--- Vue/airsoft-galery/src/components/Home.vue
+++ Vue/airsoft-galery/src/components/Home.vue
@@ -1,41 +1,8 @@
 <template>
-  <div class="hello">
-  <!--  <h1>{{ msg }}</h1>
-    <h2>lalalalaa</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Cbla bla bla bla</a></li>
-      
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div> -->
-
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toogle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div> <!-- end navbar-header-->
-
-      <div class="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-       <!--     <router-link tag="li" to="/" exact>
-              <a>home</a> 
-            </router-link> -->
-           <!-- <router-link tag="li" to="" -->
-          </ul> 
-      </div> <!-- end navbar -->
-    </div> <!-- end container -->
-  </div> <!-- end hello -->
+  <div class="home">
+ 
+    
+  </div> <!-- end home -->
 </template>
 
 <script>
