02eb3af600e8dfdcce4616c13fe9c8f9b8d1151c
Unknown <mgijon94@gmail.com>
2019-10-10 19:20:55 +0200

    still working on promises

--- Platzi .- Fundamentos de JS/lesson15.js
+++ Platzi .- Fundamentos de JS/lesson15.js
@@ -33,3 +33,33 @@ getCharacter(1)
     .catch(function(id){
         console.log(`fatall error obtainig the character id number ${id}`)
     })
+
+
+getCharacter(2)
+    .then(function(character) {
+        console.log(`${character.name}`)  
+        return getCharacter(3)
+    })
+    .then(character => {
+        console.log(`${character.name}`)  
+        return getCharacter(4)
+    })
+    .then(character =>{
+        console.log(`${character.name}`)  
+        return getCharacter(5)
+    })
+    .catch(function(id){
+        console.log(`fatal error obtainig the character id number ${id}`)
+    })
+
+var ids = [6, 7, 8, 9, 10]
+
+//var promises = ids.map(function(id){
+//    return getCharacter(id)
+//})
+
+var promises = ids.map(id => getCharacter(id)) // array of promises
+Promise
+    .all(promises)
+    .then(data => console.log(data))
+    .catch(console.log('Fatal error'))
